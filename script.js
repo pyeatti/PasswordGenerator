c2478611b15cf026f11e02329142dd29828340fd
Ian Pyeatt <pyeatt08@gmail.com>
2020-07-23 22:12:29 -0500

    created a for loop for generating a password and clear field for text area when selecting a new password generation

--- script.js
+++ script.js
@@ -1,23 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// alphabet = "abcdefghijklmnopqrstuvwxyz"
-// var alphaSplit = alphabet.split("")
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz"
+let alphabetLowerSplit = alphabetLowerCase.split("")
+alphabetLowerSplit = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// var characterSplit = specialCharacters.split("")
-specialCharacters = ["!", "@", "#", "$", "$", "$", "%", "%", "^", "&", "*", "(", ")"]
-
-
-// var alphaUpperSplit = alphabet.toUpperCase().split("")
+let alaphbetUpperSplit = alphabetLowerCase.toUpperCase().split("")
 alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+specialCharacters = "!@#$%^&*()"
+let specialCharacterSplit = specialCharacters.split("")
+specialCharacters = ["!", "@", "#", "$", "$", "$", "%", "%", "^", "&", "*", "(", ")"]
 
-// var numSplit = number.toString(10).split("")
-number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var numSplit = number.toString(10).split("")
+
+number = "1234567890"
+let numberSplit = number.split("")
+numberSplit = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+let allCharacters
 
 
-passwordLength = prompt("How many characters would you like your Password to be?")
+let password = ""
+
+// let passwordLength = prompt("How many characters would you like your Password to be?")
+
+for (let i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * alphabetLowerCase.length);
+    password = alphabetLowerCase[randomIndex]
+    console.log(password)
+    
+}
+
+
+
+
+let passwordLength = prompt("How many characters would you like your Password to be?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// passwordLength = prompt("How many characters would you like your Password to be?")
+
+// for (let index = 0; index < userInput; index++) {
+    
+//   // returns random number between 0-1
+//   let randomTinyNumber = Math.random()
+
+//   // returns random number between 0-25
+//   let randomAlphabetIndex = randomTinyNumber*alphabet.length
+
+//   let randomLetter = alphabet[randomAlphabetIndex]
+//   console.log(randomLetter)
+// }
 
 
 
@@ -51,38 +97,40 @@ while (isNaN(passwordLength) === true || passwordLength > 128 || passwordLength 
 
 
 
-// alert(passwordLength)
 
 
-lowercase = confirm("Would you lowercase letters in your Password? OK for YES, CANCEL for NO")
 
-// alert(lowercase)
+confirm("Would you lowercase letters in your Password? OK for YES, CANCEL for NO")
 
-uppercase = confirm("Would you like uppercase letters in your Password? OK for YES, CANCEL for NO")
 
-// alert(uppercase)
 
-specChar = confirm("Would you like special characters in your Password?  OK for YES, CANCEL for NO")
+confirm("Would you like uppercase letters in your Password? OK for YES, CANCEL for NO")
 
-// alert(specChar)
 
-numbers = confirm("Would you like numbers in your Password? OK for YES, CANCEL for NO")
 
-// alert(numbers)
+confirm("Would you like special characters in your Password?  OK for YES, CANCEL for NO")
+
+
+
+confirm("Would you like numbers in your Password? OK for YES, CANCEL for NO")
+
+
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  prompt("How many characters would you like your Password to be?")
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
 
-writePassword(passwordLength, alphabet, specialCharacters, alphaUpper, number);
+// writePassword(passwordLength, alphabet, specialCharacters, alphaUpper, number);
 
 
 
